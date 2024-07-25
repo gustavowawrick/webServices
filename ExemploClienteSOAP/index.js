ba1f82638520ef575fe1df85dc5630c087769c92
Gustavo Nunes <gustavo_nw@out.outlook.com>
2024-07-24 21:29:41 -0300

    Código simples apenas para manipular um client que consome um WebService

--- ExemploClienteSOAP/index.js
+++ ExemploClienteSOAP/index.js
@@ -2,5 +2,37 @@ const soap = require('soap');
 const url = 'http://www.dneonline.com/calculator.asmx?wsdl';
 
 soap.createClient(url, function (err, client) {
-    console.log('Descrever o WSDL', client.describe().Calculator.CalculatorSoap);
+    //console.log('Descrever o WSDL', client.describe().Calculator.CalculatorSoap);
+
+    client.Add({ intA: 2, intB: 2 }, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('A soma é: ' + result.AddResult);
+        }
+    });
+
+    client.Subtract({ intA: 10, intB: 20 }, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('A subtração é: ' + result.SubtractResult);
+        }
+    });
+
+    client.Multiply({ intA: 2, intB: 4 }, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('A multiplicação é: ' + result.MultiplyResult);
+        }
+    });
+
+    client.Divide({ intA: 4, intB: 2 }, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('A divisão é: ' + result.DivideResult);
+        }
+    });
 });
